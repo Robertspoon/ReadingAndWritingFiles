04bcca6b3c216f65a3b7cbb1c6f5d728e31bcb31
Robertspoon <robertthemcjug@gmail.com>
2023-02-13 13:40:41 -0400

    Initial commit

---
+++ ReadingAndWritingFiles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingAndWritingFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading and writing files");
+
+            Console.ReadKey(true);
+        }
+    }
+}
